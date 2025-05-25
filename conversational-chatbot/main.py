84a1352adaf50a6bc382eb9b195d12b477fa82bb
NaimalArain13 <naimalarain13@gmail.com>
2025-05-26 02:54:04 +0500

    added state full logic

--- conversational-chatbot/main.py
+++ conversational-chatbot/main.py
@@ -18,25 +18,32 @@ def oauth_callback(
     token: str,  
     raw_user_data: Dict[str, str], 
     default_user: cl.User, 
-) -> Optional[cl.User]:  
-    """
-    Handle the OAuth callback from GitHub
-    Return the user object if authentication is successful, None otherwise
-    """
-
-    print(f"Provider: {provider_id}")  
-    print(f"User data: {raw_user_data}")  
-
+) -> Optional[cl.User]:   
     return default_user
 
 
 @cl.on_chat_start
 async def handle_chat_start():
+    cl.user_session.set("history", [])
     await cl.Message(content="Hello! how can I help you today?").send()
 
 @cl.on_message
 async def handle_message(message: cl.Message):
-    prompt = message.content
-    response = model.generate_content(prompt)
+    history = cl.user_session.get("history")
+    history.append(
+        {"role": "user", "content": message.content}
+    )
+    formatted_history = []
+    for msg in history:
+        role = "user" if msg["role"] == "user" else "model"
+        formatted_history.append(
+            {"role": role, "parts": [{"text": msg["content"]}]}
+        )
+    response = model.generate_content(formatted_history)
+    print(response)	
     response_text = response.text if hasattr(response, "text") else ""
+    history.append(
+        {"role": "model", "content": response_text}
+    )
+    cl.user_session.set("history", history)
     await cl.Message(content=response_text).send()
